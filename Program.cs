2a35839e4e16feca888105f14bf20d99edb5dfe5
Andrew McGrath <andrew_mcgrath@outlook.com>
2024-11-25 10:43:10 +1000

    change from hash to dollar

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-// "#livedoc#" Something about livedoc
+// "$livedoc$" Something about livedoc
 // "Important!" Something important 
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
